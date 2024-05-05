ea4240cb84724c42146f51b70f8134385c64d00e
Johandre van Wyk <johandrevwyk@icloud.com>
2024-05-05 18:37:58 +0200

    excluded healthshot from rapidfire

--- RapidFire.cs
+++ RapidFire.cs
@@ -1,5 +1,6 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Entities.Constants;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace ExtraUtilities
@@ -20,56 +21,59 @@ namespace ExtraUtilities
                 if (@event.Userid is not { IsValid: true, IsHLTV: false, IsBot: false, UserId: not null, SteamID: > 0 })
                     return HookResult.Continue;
 
-                var nextPrimaryAttackTick = @event.Userid.Pawn.Value?.WeaponServices?.ActiveWeapon.Value?.NextPrimaryAttackTick ?? 0;
-                var index = @event.Userid.Pawn.Index;
-
-                if (!_lastPlayerShotTick.TryGetValue(index, out var lastShotTick))
+                if (@event.Userid.Pawn!.Value!.WeaponServices!.ActiveWeapon.Value!.DesignerName != "weapon_healthshot")
                 {
-                    _lastPlayerShotTick[index] = Server.TickCount;
-                    return HookResult.Continue;
-                }
+                    var nextPrimaryAttackTick = @event.Userid.Pawn.Value?.WeaponServices?.ActiveWeapon.Value?.NextPrimaryAttackTick ?? 0;
+                    var index = @event.Userid.Pawn.Index;
 
-                _lastPlayerShotTick[index] = Server.TickCount;
-
-                // this is ghetto but should work for now
-                if (nextPrimaryAttackTick > lastShotTick)
-                    return HookResult.Continue;
-
-                // clear list every frame (in case of misses)
-                if (_rapidFireBlockUserIds.Count == 0)
-                    Server.NextFrame(_rapidFireBlockUserIds.Clear);
-
-                _rapidFireBlockUserIds.Add(index);
-
-                // skip warning if we already warned this player in the last 3 seconds
-                if (_rapidFireBlockWarnings.TryGetValue(index, out var lastWarningTime) &&
-                    lastWarningTime + 3 > Server.CurrentTime)
-                    return HookResult.Continue;
-
-                // warn player
-
-                if (RapidFire.ContainsKey(@event.Userid.Slot)) RapidFire[@event.Userid.Slot]++;
-
-                if (RapidFire[@event.Userid.Slot] == Config.RapidFire.Threshold)
-                {
-                    string steamid = @event.Userid.SteamID.ToString();
-                    string playername = @event.Userid.PlayerName;
-                    _ = Task.Run(async () => await Discord(steamid, playername, "RapidFire"));
-
-                    if (Config.RapidFire.BanPlayer)
+                    if (!_lastPlayerShotTick.TryGetValue(index, out var lastShotTick))
                     {
-
-                        string banMessagePlayer = Chat.FormatMessage(Localizer["banmsgplayer_rapid"]);
-                        string banMessageServer = Chat.FormatMessage(Localizer["banmsgsvr_rapid", @event.Userid.PlayerName]);
-
-                        Server.ExecuteCommand($"css_ban #{@event.Userid.UserId} 0 {Localizer["banreason_rapid"]}");
-
-                        @event.Userid.PrintToChat(banMessagePlayer);
-                        Server.PrintToChatAll(banMessageServer);
+                        _lastPlayerShotTick[index] = Server.TickCount;
+                        return HookResult.Continue;
                     }
-                }
 
-                _rapidFireBlockWarnings[index] = Server.CurrentTime;
+                    _lastPlayerShotTick[index] = Server.TickCount;
+
+                    // this is ghetto but should work for now
+                    if (nextPrimaryAttackTick > lastShotTick)
+                        return HookResult.Continue;
+
+                    // clear list every frame (in case of misses)
+                    if (_rapidFireBlockUserIds.Count == 0)
+                        Server.NextFrame(_rapidFireBlockUserIds.Clear);
+
+                    _rapidFireBlockUserIds.Add(index);
+
+                    // skip warning if we already warned this player in the last 3 seconds
+                    if (_rapidFireBlockWarnings.TryGetValue(index, out var lastWarningTime) &&
+                        lastWarningTime + 3 > Server.CurrentTime)
+                        return HookResult.Continue;
+
+                    // warn player
+
+                    if (RapidFire.ContainsKey(@event.Userid.Slot)) RapidFire[@event.Userid.Slot]++;
+
+                    if (RapidFire[@event.Userid.Slot] == Config.RapidFire.Threshold)
+                    {
+                        string steamid = @event.Userid.SteamID.ToString();
+                        string playername = @event.Userid.PlayerName;
+                        _ = Task.Run(async () => await Discord(steamid, playername, "RapidFire"));
+
+                        if (Config.RapidFire.BanPlayer)
+                        {
+
+                            string banMessagePlayer = Chat.FormatMessage(Localizer["banmsgplayer_rapid"]);
+                            string banMessageServer = Chat.FormatMessage(Localizer["banmsgsvr_rapid", @event.Userid.PlayerName]);
+
+                            Server.ExecuteCommand($"css_ban #{@event.Userid.UserId} 0 {Localizer["banreason_rapid"]}");
+
+                            @event.Userid.PrintToChat(banMessagePlayer);
+                            Server.PrintToChatAll(banMessageServer);
+                        }
+                    }
+
+                    _rapidFireBlockWarnings[index] = Server.CurrentTime;
+                }               
             }
             return HookResult.Continue;
         }
